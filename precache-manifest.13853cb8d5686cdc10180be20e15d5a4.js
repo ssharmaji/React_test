a5a52dd0adf00307a28e56986d182e0e40e40387
Shivam Sharma <sharma.gwd@gmail.com>
2020-09-04 17:02:05 +0530

    Updates

---
+++ precache-manifest.13853cb8d5686cdc10180be20e15d5a4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f37622f48f3652fcd4958d8ea9b728f",
+    "url": "/React_test.git/index.html"
+  },
+  {
+    "revision": "014588fbb35f6fb5e31a",
+    "url": "/React_test.git/static/css/main.eb526241.chunk.css"
+  },
+  {
+    "revision": "065ef674370def82e40c",
+    "url": "/React_test.git/static/js/2.af6b4a5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_test.git/static/js/2.af6b4a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014588fbb35f6fb5e31a",
+    "url": "/React_test.git/static/js/main.c2344575.chunk.js"
+  },
+  {
+    "revision": "7c550ab9f056cdd4dbb3",
+    "url": "/React_test.git/static/js/runtime-main.5c84636a.js"
+  },
+  {
+    "revision": "6ca68a8ce9a5483a1feff6e99d546e62",
+    "url": "/React_test.git/static/media/cart.6ca68a8c.svg"
+  },
+  {
+    "revision": "80298ec2f4c60e778c57ccfbac9f5c64",
+    "url": "/React_test.git/static/media/img-one.80298ec2.jpg"
+  },
+  {
+    "revision": "406580c241ebc6a1e9332fdc94f2e378",
+    "url": "/React_test.git/static/media/img-three.406580c2.jpg"
+  },
+  {
+    "revision": "436d396d6efe65ff4d1b5b990db1779b",
+    "url": "/React_test.git/static/media/img-two.436d396d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_test.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7808767679ea83b1dd8eed8ebe7d508",
+    "url": "/React_test.git/static/media/menu.b7808767.svg"
+  }
+]);
